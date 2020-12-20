26052651d4f4f679a124ade57a014c2daac55901
Luis Gustavo <luis.gustavo3013@hotmail.com>
2020-12-20 15:06:57 -0300

    Created CheckIn ViewModel

---
+++ EventsApp/Model/CheckIn.swift
@@ -0,0 +1,14 @@
+//
+//  CheckIn.swift
+//  EventsApp
+//
+//  Created by Luis Gustavo Avelino de Lima Jacinto on 20/12/20.
+//
+
+import Foundation
+
+struct CheckIn: Codable, Hashable {
+  let eventId: String
+  let name: String
+  let email: String
+}
---
+++ EventsApp/ViewModel/CheckIn/CheckInViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  CheckInViewModel.swift
+//  EventsApp
+//
+//  Created by Luis Gustavo Avelino de Lima Jacinto on 20/12/20.
+//
+
+import Foundation
+
+
+
+final class CheckInViewModel {
+
+}
